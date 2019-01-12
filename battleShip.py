2ca8b4d845b30b61e375f4790c1bd1d74fa0079b
Shahab Rahnama <shahaberahnama@gmail.com>
2019-01-12 15:14:14 +0330

    Added: sets of Neighbors

--- battleShip.py
+++ battleShip.py
@@ -47,29 +47,87 @@ class battleShip:
 
         return rowSum, colSum
 
-    def makeMines(self, matrix, x, y):
-        if(matrix[x+1,y] != 1):
+    def makeMines(self, matrix, x, y, two):
+        if(not two):
+            if(matrix[x+1,y] == 1):
+                matrix[x-1,y] = 2
+            if(matrix[x-1,y] == 1):
+                matrix[x+1,y] = 2
+            if(matrix[x+1,y+1] == 1):
+                matrix[x-1,y-1] = 2
+            if(matrix[x+1,y-1] == 1):
+                matrix[x-1,y+1] = 2
+            if(matrix[x,y-1] == 1):
+                matrix[x,y+1] = 2
+            if(matrix[x,y+1] == 1):
+                matrix[x,y-1] = 2
+            if(matrix[x-1,y+1] == 1):
+                matrix[x+1,y-1] = 2
+            if(matrix[x-1,y-1] == 1):
+                matrix[x+1,y+1] = 2
+        
+
+        if(matrix[x+1,y] == 0):
             matrix[x+1,y] = -1
-        if(matrix[x-1,y] != 1):
+        if(matrix[x-1,y] == 0):
             matrix[x-1,y] = -1
-        if(matrix[x+1,y+1] != 1):
+        if(matrix[x+1,y+1] == 0):
             matrix[x+1,y+1] = -1
-        if(matrix[x+1,y-1] != 1):
+        if(matrix[x+1,y-1] == 0):
             matrix[x+1,y-1] = -1
-        if(matrix[x,y-1] != 1):
+        if(matrix[x,y-1] == 0):
             matrix[x,y-1] = -1
-        if(matrix[x,y+1] != 1):
+        if(matrix[x,y+1] == 0):
             matrix[x,y+1] = -1
-        if(matrix[x-1,y+1] != 1):
+        if(matrix[x-1,y+1] == 0):
             matrix[x-1,y+1] = -1
-        if(matrix[x-1,y-1] != 1):
+        if(matrix[x-1,y-1] == 0):
             matrix[x-1,y-1] = -1
+        
+        if(matrix[x+1,y] == 2):
+            matrix[x+1,y] = 0
+        if(matrix[x-1,y] == 2):
+            matrix[x-1,y] = 0
+        if(matrix[x+1,y+1] == 2):
+            matrix[x+1,y+1] = 0
+        if(matrix[x+1,y-1] == 2):
+            matrix[x+1,y-1] = 0
+        if(matrix[x,y-1] == 2):
+            matrix[x,y-1] = 0
+        if(matrix[x,y+1] == 2):
+            matrix[x,y+1] = 0
+        if(matrix[x-1,y+1] == 2):
+            matrix[x-1,y+1] = 0
+        if(matrix[x-1,y-1] == 2):
+            matrix[x-1,y-1] = 0
+        
+    def removeMines(self, matrix):
+        for i in range(0, self.n):
+            for j in range(0, self.n):
+                if(matrix[i,j] == -1):
+                    matrix[i,j] = 0
+        return matrix
 
 
+# 1 1 1 => 1
+# 
+# 1
+# 1
+# 1 => 2
+#
+# 1
+#   1
+#     1 => 3
+#
+#       1
+#     1
+#   1 => 4
     def makeOne(self):
         i = 0
         z = 0
         matrix = np.zeros((self.n,self.n))
+        # matrix neighbors
+        matrixN = np.zeros((self.n,self.n))
         while(i < self.all):
             x= np.random.randint(0,(self.n)-1)
             y= np.random.randint(0,(self.n)-1)
@@ -78,11 +136,41 @@ class battleShip:
             # while(j < self.col[z]):
             self.checkCellNeighbors()
             self.env = matrix
-            if((matrix[x,y] != 1)  ):#and (self.row[x] != 0) and (self.col[y] != 0)):
+            if((matrix[x,y] != 1)): # and (matrix[x,y] != -1)  ):#and (self.row[x] != 0) and (self.col[y] != 0)):
                 if(self.cellNeighbors[x,y] == 1):
-                    matrix[x,y] = 1
-                    # self.makeMines(matrix, x, y)
-                    i+=1
+                    if(((matrix[x-1,y-1] == 1) and (matrixN[x-1,y-1] == 3 or matrixN[x-1,y-1] == 3)) or ((matrix[x+1,y+1] == 1) and (matrixN[x+1,y+1] == 3 or matrixN[x+1,y+1] == 0))):
+
+                        matrix[x,y] = 1
+                        matrixN[x,y] = 3
+                        # self.makeMines(matrix, x, y, True)
+
+                        # self.makeMines(matrix, x, y)
+                        i+=1
+                    elif(((matrix[x-1,y+1] == 1 ) and (matrixN[x-1,y+1] == 4 or matrixN[x-1,y+1] == 0)) or ((matrix[x+1,y-1] == 1) and (matrixN[x+1,y-1] == 4 or matrixN[x+1,y-1] == 0))):
+
+                        matrix[x,y] = 1
+                        matrixN[x,y] = 4
+                        # self.makeMines(matrix, x, y, True)
+
+                        # self.makeMines(matrix, x, y)
+                        i+=1
+                    elif(((matrix[x-1,y] == 1) and (matrixN[x-1,y] == 2 or matrixN[x-1,y] == 0)) or ((matrix[x+1,y] == 1) and (matrixN[x+1,y] == 2 or matrixN[x+1,y] == 0))):
+
+                        matrix[x,y] = 1
+                        matrixN[x,y] = 2
+                        # self.makeMines(matrix, x, y, True)
+
+                        # self.makeMines(matrix, x, y)
+                        i+=1
+                    elif(((matrix[x,y-1] == 1) and (matrixN[x,y-1] == 1 or matrixN[x,y-1] == 0)) or ((matrix[x,y+1] == 1) and (matrixN[x,y+1] == 1 or matrixN[x,y+1] == 0))):
+
+                        matrix[x,y] = 1
+                        matrixN[x,y] = 1
+                        # self.makeMines(matrix, x, y, True)
+
+                        # self.makeMines(matrix, x, y)
+                        i+=1
+                    
                 elif(self.cellNeighbors[x,y]==0):
                     matrix[x,y] = 1
                     # self.makeMines(matrix, x, y)
@@ -91,30 +179,64 @@ class battleShip:
                     if((x == 0 and y == 0) or (x == 0 and y == self.n-1) or (x == self.n-1 and y == self.n-1) or (x == self.n-1 and y == 0)):
                         continue
                     elif(x == 0 and y > 0):
-                        if(matrix[x,y-1] + matrix[x,y+1]==2):
+                        if((matrix[x,y-1] + matrix[x,y+1] == 2) and (matrixN[x,y-1] == 1) and (matrixN[x,y+1] == 1)):
                             matrix[x,y] = 1
-                            # self.makeMines(matrix, x, y)
+                            matrixN[x,y] = 1
+                            # self.makeMines(matrix, x, y, True)
                             i+=1
                     elif(x > 0 and y == 0):
-                        if(matrix[x-1,y] + matrix[x+1,y] == 2):
+                        if((matrix[x-1,y] + matrix[x+1,y] == 2) and (matrixN[x-1,y] == 2) and (matrixN[x+1,y] == 2)):
                             matrix[x,y] = 1
+                            matrixN[x,y] = 2
+                            # self.makeMines(matrix, x, y, True)
                             # self.makeMines(matrix, x, y)
                             i+=1
                     elif(x == self.n-1 and y > 0):
-                        if(matrix[x,y-1] + matrix[x,y+1]==2):
+                        if((matrix[x,y-1] + matrix[x,y+1] == 2) and (matrixN[x,y-1] == 1) and (matrixN[x,y+1] == 1)):
                             matrix[x,y] = 1
+                            matrixN[x,y] = 1
                             i+=1
+                            # self.makeMines(matrix, x, y, True)
                             # self.makeMines(matrix, x, y)
                     elif(x > 0 and y == self.n-1):
-                        if(matrix[x-1,y] + matrix[x+1,y] == 2):
+                        if((matrix[x-1,y] + matrix[x+1,y] == 2) and (matrixN[x-1,y] == 2) and (matrix[x+1,y] == 2)):
                             matrix[x,y] = 1
+                            matrixN[x,y] = 2
+                            # self.makeMines(matrix, x, y, True)
                             # self.makeMines(matrix, x, y)
                             i+=1
 
                     else:
-                        if((matrix[x-1,y-1] + matrix[x+1,y+1] == 2) or (matrix[x-1,y+1] + matrix[x+1,y-1] == 2) or (matrix[x-1,y] + matrix[x+1,y] == 2) or (matrix[x,y-1] + matrix[x,y+1] == 2)):
+                        if((matrix[x-1,y-1] + matrix[x+1,y+1] == 2) and (matrixN[x-1,y-1] == 3) and (matrixN[x+1,y+1] == 3)):
 
                             matrix[x,y] = 1
+                            matrixN[x,y] = 3
+                            # self.makeMines(matrix, x, y, True)
+
+                            # self.makeMines(matrix, x, y)
+                            i+=1
+                        elif((matrix[x-1,y+1] + matrix[x+1,y-1] == 2) and (matrixN[x-1,y+1] == 4) and (matrixN[x+1,y-1] == 4)):
+
+                            matrix[x,y] = 1
+                            matrixN[x,y] = 4
+                            # self.makeMines(matrix, x, y, True)
+
+                            # self.makeMines(matrix, x, y)
+                            i+=1
+                        elif((matrix[x-1,y] + matrix[x+1,y] == 2) and (matrixN[x-1,y] == 2) and (matrixN[x+1,y] == 2)):
+
+                            matrix[x,y] = 1
+                            matrixN[x,y] = 2
+                            # self.makeMines(matrix, x, y, True)
+
+                            # self.makeMines(matrix, x, y)
+                            i+=1
+                        elif((matrix[x,y-1] + matrix[x,y+1] == 2) and (matrixN[x,y-1] == 1) and (matrixN[x,y+1] == 1)):
+
+                            matrix[x,y] = 1
+                            matrixN[x,y] = 1
+                            # self.makeMines(matrix, x, y, True)
+
                             # self.makeMines(matrix, x, y)
                             i+=1
                 elif(self.cellNeighbors[x,y] > 2):
@@ -123,17 +245,21 @@ class battleShip:
 
                 # j+=1
             # i+=1
+        # matrix = self.removeMines(matrix)
         print(matrix)
-        print(self.cellNeighbors )
-        return matrix
+        print(matrixN)
+        # print(self.cellNeighbors )
+        
         self.env = matrix
+        return matrix
         # print(matrix[3,3])
 
+# sum all neighbors
     def checkCellNeighbors(self):
         matrix = self.env
         # print(matrix)
         self.cellNeighbors = signal.convolve2d(matrix, np.ones((3,3)), mode='same')
-        # print(self.cellNeighbors)
+        # print("cellneighbors", self.cellNeighbors)
         # a = signal.convolve2d(matrix, np.ones((2,1)), mode='same')
         # print(a)
         # c = np.roll(matrix,1,0)
@@ -200,7 +326,7 @@ class battleShip:
             myDic= { "matrix": matrix, "row":row, "col": col , "rowSum": sum([ abs(x) for x in row]), "colSum": sum([ abs(x) for x in col])   }
 
             self.population[i] = myDic
-        print(self.population[1])
+        # print(self.population[1])
     def fitness(self):
         print("fitness")
     def crossOver(self):
@@ -209,7 +335,7 @@ def main():
 
     row = [2,4,1,1,2,4,1,4]
     col = [3,1,4,1,2,3,0,5]
-    populationCounts = 10
+    populationCounts = 2
     bs = battleShip(8, col, row, populationCounts)
     bs.makePopulation()
     # bs.makeOne()
